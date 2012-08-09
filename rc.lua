92fac4de55740464a0c3a910ea78f248ba3cef9e
Unknown-Anomaly <riedel.a.walden@gmail.com>
2012-08-09 00:08:31 -0400

    Latest commit, added conkyrc

--- rc.lua
+++ rc.lua
@@ -33,7 +33,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/home/molganvk/.config/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
