b28705393cbc9560574a4742edf125fd67698db8
Unknown-Anomaly <riedel.a.walden@gmail.com>
2012-09-12 11:48:47 -0400

    Finished wallpaperchanger.sh

--- scripts/wallpaperchanger.sh
+++ scripts/wallpaperchanger.sh
@@ -1,9 +1,29 @@
-#! /bin/sh
+#! /bin/bash
 
 # Author: Walden Riedel
 # Title: Cross wallpaper changer for users of awesome wm
 # Description: This script uses chron to run every differently alloted time period.
 #              Uses feh to work along with the default functions awsetbg of awesome,
 #              */5 * * * * /home/molganvk/wallpaperchanger.sh
+# 
+#              Note: Mainly uses *.png files, all these file directories are used symbolically,
+#                    Change how you see fit to use.
+
+Local=$HOME/Dropbox/Pictures/Desktop_Pictures/
+LocalDir=$HOME/Dropbox/Pictures/Desktop_Pictures/*
+DesktopImg=$HOME/Pictures/DesktopImg.png
 
 
+cd $Local
+RandInt=$(find . -type f | wc -l)
+FileNum=$[ ( $RANDOM % $RandInt ) ]
+Counter=0
+
+for f in $LocalDir
+do
+	if [ $Counter == $FileNum ];then
+		echo "Picture $f"
+		cp $f $DesktopImg
+	fi
+	let "Counter = ${Counter} + 1"
+done
