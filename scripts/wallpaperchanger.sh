7b11f9b9ec70b1b0d935e4ad9054f00939838e8d
Unknown-Anomaly <riedel.a.walden@gmail.com>
2012-09-12 08:59:52 -0400

    Added wallpaperchanger.sh

---
+++ scripts/wallpaperchanger.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# Author: Walden Riedel
+# Title: Cross wallpaper changer for users of awesome wm
+# Description: This script uses chron to run every differently alloted time period.
+#              Uses feh to work along with the default functions awsetbg of awesome,
+#              */5 * * * * /home/molganvk/wallpaperchanger.sh
+
+
