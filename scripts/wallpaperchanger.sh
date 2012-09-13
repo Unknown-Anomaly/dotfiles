7b07b703080455eb9f2eaac06a84e97b68204855
Unknown-Anomaly <riedel.a.walden@gmail.com>
2012-09-13 00:00:38 -0400

    Fixed/tweaked wallpaperchanger.sh and small edit with xinitrc What I did was fix my glitch with crontab executing my scripts and not fully switch the displayed background, using sleep X in my wallpaperchanger script I can achieve this jointly with my xinitrc starting my wallpaperchanger script.

--- scripts/wallpaperchanger.sh
+++ scripts/wallpaperchanger.sh
@@ -5,30 +5,38 @@
 # Description: This script uses chron to run every differently alloted time period.
 #              Uses feh to work along with the default functions awsetbg of awesome,
 #              */5 * * * * /home/molganvk/wallpaperchanger.sh
+#              Also I added this file into my xinitrc when I start awesome as to facilitate changing wallpapers with awesome alone.
 # 
 #              Note: Mainly uses *.png files, all these file directories are used symbolically,
 #                    Change how you see fit to use.
 # Author Contact: war4756@gmail.com or github.com/Unknown-Anomaly
 #
 
-Local=$HOME/Dropbox/Pictures/Desktop_Pictures/ # This variable for the folder address of the pictures you want to go through.
-LocalDir=$HOME/Dropbox/Pictures/Desktop_Pictures/* # This variable is for all the files inside the folder you want this script to go through.
-DesktopImg=$HOME/Pictures/DesktopImg.png # This is the image that gets over-written to change your background.
-
-cd $Local
-RandInt=$(find . -type f | wc -l)
-FileNum=$[ ( $RANDOM % $RandInt ) ]
-Counter=1
-
-for f in $LocalDir
+while [ x$DISPLAY != x ]
 do
-	if [ $Counter == $FileNum ]; then
-		## One could instead of copying the image just use 'awsetbg $DesktopImg'
-		# it would work just as well, but when machine reloads it captures where-ever
-		# you set your desktop in ~/.config/awesome/theme/default/theme.lua this way
-		# so when awesome reloads you get the previous desktop background you had.
-		cp $f $DesktopImg 
-		awsetbg $DesktopImg
-	fi
-	let "Counter = ${Counter} + 1"
+	Local=$HOME/Dropbox/Pictures/Desktop_Pictures/ # This variable for the folder address of the pictures you want to go through.
+	LocalDir=$HOME/Dropbox/Pictures/Desktop_Pictures/* # This variable is for all the files inside the folder you want this script to go through.
+	DesktopImg=$HOME/Pictures/DesktopImg.png # This is the image that gets over-written to change your background.
+
+	sleep 120 # Number of seconds this script waits until executing again.
+
+	cd $Local
+	RandInt=$(find . -type f | wc -l)
+	FileNum=$[ ( $RANDOM % $RandInt ) ]
+	Counter=1
+	
+	for f in $LocalDir
+	do
+		if [ $Counter == $FileNum ]; then
+			## One could instead of copying the image just use 'awsetbg $DesktopImg'
+			# it would work just as well, but when machine reloads it captures where-ever
+			# you set your desktop in ~/.config/awesome/theme/default/theme.lua this way
+			# so when awesome reloads you get the previous desktop background you had.
+			cp $f $DesktopImg
+			awsetbg $DesktopImg
+		fi
+		let "Counter = ${Counter} + 1"
+	done
+	
+	notify-send "Background Changed"
 done
