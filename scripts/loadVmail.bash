59f476bdc7d8db46eb5f2525aa5f99f7e162d1e0
Walden Riedel <riedel.a.walden>
2012-12-18 18:59:39 -0500

    Updated loadVmail.bash to better handle not being in the native PATH.

--- scripts/loadVmail.bash
+++ scripts/loadVmail.bash
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Set load defaults
-export VMAIL_BROWSER="elinks"
-export VMAIL_HTML_PART_READER="elinks -dump"
+export VMAIL_BROWSER="links"
+export VMAIL_HTML_PART_READER="links -dump"
+
+# Add Ruby gems executables to PATH
+PATH=$PATH:$HOME/.gem/ruby/1.9.1/
+export PATH
 
 # Start vmail
 vmail
