a65ef20a3904b6c8a1169994b4da17cda37734c0
Walden Riedel <riedel.a.walden>
2012-12-19 15:53:44 -0500

    Updated loadVmail.bash

--- scripts/loadVmail.bash
+++ scripts/loadVmail.bash
@@ -4,9 +4,5 @@
 export VMAIL_BROWSER="links"
 export VMAIL_HTML_PART_READER="links -dump"
 
-# Add Ruby gems executables to PATH
-PATH=$PATH:$HOME/.gem/ruby/1.9.1/
-export PATH
-
-# Start vmail
-vmail
+# Add Ruby gems executables to PATH, then run.
+echo -e "Please type and execute\n\nPATH=\$PATH:\$HOME/.gem/ruby/1.9.1/\nexport PATH\nvmail"
