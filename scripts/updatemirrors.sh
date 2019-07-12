8110885dd4bf7e5061eacbeec77f2e20b9066d37
Molgan Kiaqol <molganvkiaqol@gmail.com>
2019-07-12 16:33:48 -0400

    Updated script updatemirrors.sh to be more useful.

--- scripts/updatemirrors.sh
+++ scripts/updatemirrors.sh
@@ -1,22 +1,54 @@
 #!/bin/sh
 
-[ "$UID" != 0 ] && su=sudo
+# url="https://www.archlinux.org/mirrorlist/?country=$country&protocol=http&use_mirror_status=on"
 
-country='US'
-url="http://www.archlinux.org/mirrorlist/?country=$country&protocol=ftp&protocol=http&ip_version=4&use_mirror_status=on"
+# Only run if privledged.
+if [[ $(id -u) != 0 ]]; then
+   echo -e "Error: Permission Denied."
+   exit 1
+fi
 
-# Get latest mirror list and save to tmpfile
-$su wget -O /etc/pacman.d/mirrorlist.orig https://www.archlinux.org/mirrorlist/all/http
+# Assign defaults.
+charCountry="US" # Assign the default country code.
+numMirrors=5 # Assign the default number.
+
+if [[ ${#1} == "2" ]]; then
+   if [[ $1 =~ ^[A-Z]+$ ]]; then
+      echo -e "Using provided country code: $1."
+      charCountry=$1
+   else
+      echo -e "Using default country code: $charCountry."
+   fi
+else
+   echo -e "Using default country code: $charCountry."
+fi
+
+if [[ ${#2} == "1" || ${#2} == "2" ]]; then
+   if [[ $2 =~ ^[0-9]+$ ]]; then
+      echo -e "Using provided number of mirrors: $2."
+      numMirrors=$2
+   else
+      echo -e "Using default number of mirrors: $numMirrors."
+   fi
+else
+   echo -e "Using default number of mirrors: $numMirrors."
+fi
+
+echo -e "\n"
+
+# Get latest mirror list and save to a tmpfile
+$su curl -o /etc/pacman.d/mirrorlist.orig "https://www.archlinux.org/mirrorlist/?country=$charCountry&protocol=http&use_mirror_status=on"
+
+echo -e "\nProcessing and backing up downloaded server list.\n"
 $su sed '/^#\S/ s|#||' -i /etc/pacman.d/mirrorlist.orig
+$su cp /etc/pacman.d/mirrorlist.orig /etc/pacman.d/mirrorlist.backup
+$su rm /etc/pacman.d/mirrorlist.orig
 
-$su cp /etc/pacman.d/mirrorlist.orig /etc/pacman.d/mirrorlist
+echo -e "Running rankmirrors."
+$su rankmirrors -n $numMirrors /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+echo -e "Done.\n"
 
-# Run rankmirrors
-echo " Backing up mirrorlist..."
-$su cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-echo " Running rankmirrors..."
-$su rankmirrors -n 3 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
-
-$su cat /etc/pacman.d/mirrorlist
-
-echo "Please run $ sudo pacman -Syy"
+# Display servers used.
+echo -e "These servers will be used."
+$su tail -n$numMirrors /etc/pacman.d/mirrorlist
+echo -e "\nPlease run $ sudo pacman -Syy"
